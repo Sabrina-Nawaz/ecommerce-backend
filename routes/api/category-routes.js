9837dd62223445697bac5f908ab5c57d65b9214f
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-11-27 20:08:50 -0500

    Created routes to get all categories and find one category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,15 +1,43 @@
 const router = require('express').Router();
+const { restart } = require('nodemon');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      attributes: ["id", "category_name"],
+      // be sure to include its associated Products
+      include: [
+        {
+          model: Product, attributes: ["id", "product_name", "price", "stock", "category_id"],
+        },
+      ]
+    });
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product, attributes: ["id", "product_name", "price", "stock", "category_id"],
+      }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    restart.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
