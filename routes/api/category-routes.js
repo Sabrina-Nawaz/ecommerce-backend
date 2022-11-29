276eb1d7c558a32e19f8f1e4693acf043c81eb00
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-11-29 12:14:23 -0500

    Completed posting, putting, updating and deleting in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,32 +25,65 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
-    const categoryData = await Category.findByPk(req.params.id, {
+    const categoryId = await Category.findOne({
+      // be sure to include its associated Products
+      where: { id: req.params.id },
       include: [{
         model: Product, attributes: ["id", "product_name", "price", "stock", "category_id"],
       }]
     });
-    if (!categoryData) {
+    if (!categoryId) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json(categoryId);
   } catch (err) {
     restart.status(500).json(err);
   }
-  // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name,
+  },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((updatedCategory) => {
+    res.json(updatedCategory);
+  })
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!deletedCategory) {
+      res.status(404).json({ message: "No category with this id!" });
+      return;
+    }
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
